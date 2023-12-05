332c061bb934257efc2d0499cb9cc5c4171e6985
Majestic Fire <113261194+Majestic-Fire@users.noreply.github.com>
2023-12-06 00:31:28 +0800

    add template

--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 ---
 - last occurence
   - `re.search("(?s:.*)pattern", "target_text")`
+
+#### Day 2
+---
+- Used Class Approach (could be just dataclass[struct])
+
+#### Day 3
+---
+- simplify naming
+- loop
+``` python
+for cr in [row-1, row, row+1]:
+    for cc in [col-1, col, col+1]:
+```
+
+#### Day 4
+---
+- match 2 list
+```
+count = sum(n in winNum for n in num)
+```
---
+++ template.py
@@ -0,0 +1,62 @@
+def part1(input):
+
+    return 0
+
+
+def part2(input):
+    
+    return 0
+
+
+
+##### Main #####
+
+import os
+from dotenv import load_dotenv
+import requests
+
+
+if __name__ == "__main__":
+    
+    part1_enabled = False
+    # part1_enabled = True
+    part2_enabled = False
+    # part2_enabled = True
+
+    day = 3
+    year = 2023
+
+    with open(f"day{day}/sample_input.txt", "r") as file:
+        arr = [line.strip() for line in file.readlines()]   
+    result = part1(arr)
+    print(f"[Sample]This is the part-1 result: {result}")
+    result = part2(arr)
+    print(f"[Sample]This is the part-2 result: {result}")
+
+
+
+# Load the environment variables from the .env file
+load_dotenv()
+# Get the session token from the environment variables
+session_token = os.getenv('AOC_SESSION_COOKIE')
+# URL
+url = f"https://adventofcode.com/{year}/day/{day}/input"
+headers = {
+    'User-Agent': 'Mozilla/5.0',
+}
+
+response = requests.get(url, cookies={'session': session_token}, headers=headers)
+
+
+if response.status_code == 200:
+    data = response.text
+    # Process the data as needed
+    if part1_enabled:
+
+        result = part1(data.splitlines())
+        print(f"[Puzzle]This is the part-1 result: {result}")
+    if part2_enabled:
+        result = part2(data.splitlines())
+        print(f"[Puzzle]This is the part-2 result: {result}")
+else:
+    print("Error: Failed to fetch data. Status code:", response.status_code)
