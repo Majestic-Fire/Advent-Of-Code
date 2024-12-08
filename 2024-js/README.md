225a668ef869556dcbce452dda2f46cef6005629
Majestic Fire <113261194+Majestic-Fire@users.noreply.github.com>
2024-12-08 12:27:56 +0800

    day 7 done

--- 2024-js/README.md
+++ 2024-js/README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -104,16 +104,16 @@ Both parts: 18.435ms
 
 ```
 Day 06
-Time part 1: 3.445ms
-Time part 2: 6345.874ms
-Both parts: 6349.319ms
+Time part 1: 2.362ms
+Time part 2: 6736.204ms
+Both parts: 6738.566ms
 ```
 
 ```
 Day 07
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 82.768ms
+Time part 2: 6644.3ms
+Both parts: 6727.068ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 12/50
-Total time: 6398.68ms
+Total stars: 14/50
+Total time: 13514.996ms
 ```
 
 <!--/RESULTS-->
--- 2024-js/src/day07/index.js
+++ 2024-js/src/day07/index.js
@@ -50,9 +50,50 @@ const part1 = (rawInput) => {
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
+  const lines = input.split("\n");
+
+  const eqns = [];
+  lines.forEach(line => {
+    const [key, values] = line.split(":").map(x => x.trim());
+    eqns.push({ k: Number(key), v: values.split(" ").map(Number) });
+  });
+
+  const evaluate = (v, operators) => {
+    let product = v[0];
+    operators.forEach((op, i) => {
+      if (op === '+') {
+        product += v[i + 1];
+      } else if (op === '*') {
+        product *= v[i + 1];
+      } else if (op === '||') {
+        product = product * Math.pow(10, v[i + 1].toString().length) + v[i + 1];
+      }
+    });
+    return product;
+  };
+
+  function findOperators(v, k, operators) {
+    // if found 
+    if (operators.length === v.length - 1) {
+      const result = evaluate(v, operators);
+      return result === k;
+    }
+
+    // recursion
+    return (
+      findOperators(v, k, [...operators, '||']) ||
+      findOperators(v, k, [...operators, '+']) ||
+      findOperators(v, k, [...operators, '*'])
+    )
+  }
+
 
   let result = 0;
-
+  eqns.forEach(({ k, v }) => {
+    if (findOperators(v, k, [])) {
+      result += k;
+    }
+  });
   return result;
 };
 
@@ -86,7 +127,7 @@ run({
 192: 17 8 14
 21037: 9 7 18 13
 292: 11 6 16 20`,
-        expected: "",
+        expected: 11387,
       },
     ],
     solution: part2,
